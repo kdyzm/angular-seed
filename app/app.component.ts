529e48ad17cb464bb7ac7a4089e093356de8296f
kdyzm <1649653731@qq.com>
2018-08-26 14:12:12 +0800

    使用navigate方法导航

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component} from "@angular/core";
+import {Router} from "@angular/router";
 
 @Component(
     {
@@ -6,9 +7,16 @@ import {Component} from "@angular/core";
         template: `
             <a [routerLink]="['/']">Home</a>
             <a [routerLink]="['/product']">Product Detail</a>
+            <input type="button" value="Product Detail" (click)="navigateToProductDetail()"/>
             <router-outlet></router-outlet>
         `
     }
 )
 export class AppComponent {
+    constructor(private _router:Router){
+
+    }
+    navigateToProductDetail(){
+        this._router.navigate(["/product"]);
+    }
 }
